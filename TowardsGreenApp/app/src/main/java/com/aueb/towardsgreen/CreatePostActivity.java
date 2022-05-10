c0434a7685506803a12c07f471b390b0505fe157
xristoulakhs <antonisxania55@gmail.com>
2022-05-10 07:36:43 -0500

    first changes and inits

---
+++ TowardsGreenApp/app/src/main/java/com/aueb/towardsgreen/CreatePostActivity.java
@@ -0,0 +1,14 @@
+package com.aueb.towardsgreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post);
+    }
+}
