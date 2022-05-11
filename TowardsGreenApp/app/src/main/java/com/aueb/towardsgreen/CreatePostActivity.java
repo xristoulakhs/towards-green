1f665b55e1d4ea1c7704c6c29bdaae7f4c28ea9f
xristoulakhs <antonisxania55@gmail.com>
2022-05-11 07:46:15 -0500

    create profile, create post, profile uis

--- TowardsGreenApp/app/src/main/java/com/aueb/towardsgreen/CreatePostActivity.java
+++ TowardsGreenApp/app/src/main/java/com/aueb/towardsgreen/CreatePostActivity.java
@@ -3,12 +3,27 @@ package com.aueb.towardsgreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class CreatePostActivity extends AppCompatActivity {
+public class CreatePostActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+        String[] locationList={"Athens, Chania"};
+        ListView listView= (ListView) findViewById(R.id.locationList);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
