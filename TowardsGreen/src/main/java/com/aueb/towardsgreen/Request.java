bac1c4698ed0f9b75520e33fb88f31c1784e4394
Apipilikas <apipilikas@gmail.com>
2022-05-24 12:31:35 +0300

    added Server side main implementation

---
+++ TowardsGreen/src/main/java/com/aueb/towardsgreen/Request.java
@@ -0,0 +1,25 @@
+package com.aueb.towardsgreen;
+
+
+import java.io.Serializable;
+
+public class Request implements Serializable {
+
+	private static final long serialVersionUID = 46L;
+	
+	private String requestType;
+	private String content;
+	
+	public Request(String requestType, String content) {
+		this.requestType = requestType;
+		this.content = content;
+	}
+
+	public String getRequestType() {
+		return requestType;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
