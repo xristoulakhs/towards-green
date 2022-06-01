a01985e9d14ff1a65ff33f382169047bd778dab1
Apipilikas <apipilikas@gmail.com>
2022-06-01 15:15:12 +0300

    resolved merging conflicts left in code

--- TowardsGreenApp/app/src/main/java/com/aueb/towardsgreen/domain/Profile.java
+++ TowardsGreenApp/app/src/main/java/com/aueb/towardsgreen/domain/Profile.java
@@ -9,7 +9,7 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
+//import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
@@ -97,7 +97,7 @@ public class Profile {
         //MultiFormatWriter is a factory class that finds the appropriate Writer subclass
         // for the BarcodeFormat requested and encodes the barcode with the supplied contents.
         BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, width, height);
-        MatrixToImageWriter.writeToPath(matrix,"png", Paths.get(path+userFullName+".png"));
+        //MatrixToImageWriter.writeToPath(matrix,"png", Paths.get(path+userFullName+".png"));
     }
 
     public void generateUserId() {
